e99e9ff543483fe9942664cd4cb0d3844e4ee301
Jérôme Ludmann <jerome@ludmann.io>
2016-08-26 19:22:02 +0200

    Adding ES7 features

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
-import 'source-map-support/register';
+import "source-map-support/register";
 import {ExampleImpl} from "./ExampleImpl";
 
 const stringUtil = new ExampleImpl("Hello World");
 console.log(stringUtil.capitalize());
-
-stringUtil.value = "new string";
 console.log(stringUtil.getLength());
-
 stringUtil.value = null;
 console.log(stringUtil.isEmpty());
+
+async function call() {
+    await Promise.resolve("test");
+}
+
+call();
